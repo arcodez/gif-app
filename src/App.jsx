aa96690d8cf7cb6ba0472ea247a64d4593079b42
arcodez <abelileitor123@gmail.com>
2021-06-30 20:54:26 -0400

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React from "react";
 import { ListOfGif } from "./components/ListOfGif";
-import { useGetGifs } from "./hooks/useGetGifs";
 import "./styles/styles.css";
 
 export default function App() {
-  const { searchTerm, handleChange } = useGetGifs;
   return (
     <div className="galeria">
       <h1>App de Gifs</h1>
-
-      <input type="text" value={searchTerm} onChange={handleChange} />
-      <div className="linea"></div>
       <ListOfGif />
     </div>
   );
--- src/components/ListOfGif/index.jsx
+++ src/components/ListOfGif/index.jsx
@@ -4,18 +4,24 @@ import { Gif } from "../Gif";
 import "./style.css";
 
 export function ListOfGif() {
-  const { loading, gifs } = useGetGifs(undefined);
+  const { loading, gifs, searchTerm, handleChange } = useGetGifs(undefined);
   return (
-    <div className="contenedor-imagenes">
-      {loading ? (
-        <p>cargando</p>
-      ) : (
-        gifs.map((g, index) => (
-          <Fragment key={g.id}>
-            <Gif index={index} g={g} />
-          </Fragment>
-        ))
-      )}
-    </div>
+    <Fragment>
+      <div className="buscador">
+        <input type="text" value={searchTerm} onChange={handleChange} />
+      </div>
+      <div className="linea"></div>
+      <div className="contenedor-imagenes">
+        {loading ? (
+          <p>cargando</p>
+        ) : (
+          gifs.map((g, index) => (
+            <Fragment key={g.id}>
+              <Gif index={index} g={g} />
+            </Fragment>
+          ))
+        )}
+      </div>
+    </Fragment>
   );
 }
